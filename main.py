b0bf77d883e5b86089f274e2e2bdcd804574d0c1
ardaozennn <ardacan.ozen@std.yeditepe.edu.tr>
2024-12-26 16:40:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+alt=float(input("wie alt sind sie"))
+if 50 > alt > 20 :
+  abschlussnote=float(input("Was ist deine Abschlussnote"))
+if abschlussnote >= 80 :
+  print ("Einstellen")
+  if abschlussnote <80 :
+    print ("Ablemen")
+    
+ 
+
+  
+  
